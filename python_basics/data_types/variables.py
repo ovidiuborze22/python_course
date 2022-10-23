2418aec307103a971c06ea42b9008a942def96ed
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-23 21:47:47 +0100

    variables

---
+++ python_basics/data_types/variables.py
@@ -0,0 +1,21 @@
+#understanding the use of variables
+
+#EXAMPLE 1:
+spent = 3
+donated = 4
+
+total_amount = spent + donated
+
+print(total_amount)
+
+
+#EXAMPLE 2
+items = 10
+price = 2
+
+total_price = items * price
+
+print(total_price)
+
+#Bellow we print put three variables
+print(total_price, items, price)
