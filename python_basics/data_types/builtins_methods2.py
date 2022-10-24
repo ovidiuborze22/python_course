e80d206531dd02e9eb3e039c988f49423013ea89
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-24 21:20:11 +0100

    counting values

---
+++ python_basics/data_types/builtins_methods2.py
@@ -0,0 +1,6 @@
+# Below you can see a student_grades list. 
+# Your task is to use Python to count how many items with a value of 10.0 are in the student_grades list. 
+# Once you find the proper list method that counts items, then print out its output using a print() function.
+
+student_grades = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9, 10.0]
+print(student_grades.count(10.0))
