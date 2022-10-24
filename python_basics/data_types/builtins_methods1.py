64fae0b6b74bb3ef8091522e6d66e9c4b1e623b9
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-24 21:15:50 +0100

    calculating the mean

---
+++ python_basics/data_types/builtins_methods1.py
@@ -0,0 +1,10 @@
+# Complete line 2 of the code so that it prints out the maximum value of student_grades. 
+# In other words, your duty is to find out the appropriate function or method that returns 
+# the maximum value of a list and store its output in max_value.
+
+#Tip: You can use the introspective dir() function on your local IDE to find out 
+# the function/method that returns the maximum value.
+
+student_grades = [9.1, 8.8, 7.5]
+max_value = max(student_grades)
+print(max_value)
