2a9ee3f05d663b725f4548c77fc58eebc017566f
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-24 21:22:39 +0100

    modifying a string

---
+++ python_basics/data_types/builtins_methods3.py
@@ -0,0 +1,6 @@
+# Like the previous exercise, find the proper function or method 
+# that converts the string in username into lowercase letters and prints out the output
+
+username = "Python3"
+
+print(username.lower())
