76c797462fa7191ec5549d48b036ab870f89c1f3
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-24 21:46:59 +0100

    dictionary types

---
+++ python_basics/data_types/dictionary_types.py
@@ -0,0 +1,7 @@
+monday_temperatures = [9.1, 8.8, 7.5]
+students_grades = {"Marry": 9.1, "Sim": 8.8, "John": 7.5}
+
+my_sum = sum(students_grades.values())
+length = len(students_grades)
+mean = my_sum / length
+print(mean)
