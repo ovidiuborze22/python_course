2fcf4a79b0cc5f539a5430b3cd5031ce36d899b5
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-24 20:30:40 +0100

    math operators

--- python_basics/math_operators/math_operators.py
+++ python_basics/math_operators/math_operators.py
@@ -13,14 +13,14 @@ print(3 / 4)
 # Floor Division //
 print(9 // 2)
 # Explanation: If your room is nine meters high,
-# you could place four 2-meter-high wardrobes on top of each other.
+# you could place four 2-meter-high wardrobes on top of each other
 
 # Modulus %
 print(9 % 2)
 # Explanation: If your room is nine meters high, 
-# you could place four 2-meter-high wardrobes on top of each other. 
+# you could place four 2-meter-high wardrobes on top of each other
 # There would be one meter of vertical space left.
 
 # Exponentiation
 print( 3 ** 4)
-# Explanation: 3 ** 4 is the equivalent of 3 * 3 * 3 * 3.
+# Explanation: 3 ** 4 is the equivalent of 3 * 3 * 3 * 3
