734db68b60c30de416afd679e7dee01761926d08
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-24 21:10:35 +0100

    calculating the mean

---
+++ python_basics/data_types/builtins_methods.py
@@ -0,0 +1,8 @@
+# using builtins methods
+students_grades = [9.1, 8.8, 7.5]
+
+# calculating the mean
+mysum = sum(students_grades)
+length = len(students_grades)
+mean = mysum / length
+print(mean)
