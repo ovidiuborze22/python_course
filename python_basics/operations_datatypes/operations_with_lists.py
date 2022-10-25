6b53bb022fbd0b143df9a4de0ab93f88b561eda0
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-25 22:12:40 +0100

    operations with lists

---
+++ python_basics/operations_datatypes/operations_with_lists.py
@@ -0,0 +1,13 @@
+# .append() to append 9.9 another value to the list
+
+monday_temperatures = [9.1, 8.8, 7.5]
+monday_temperatures.append(9.9)
+print(monday_temperatures)
+
+# .clear() will clear the list
+monday_temperatures.clear()
+print(monday_temperatures)
+
+# .index() will display the index of a value from the list
+monday_temperatures = [9.1, 8.8, 7.5]
+print(monday_temperatures.index(8.8))
