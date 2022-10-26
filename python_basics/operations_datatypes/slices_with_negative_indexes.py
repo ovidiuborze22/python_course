559da5ea6f860f7decaf8442257be3409ee555ea
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-26 23:00:06 +0100

    accessing items and slices with negative indexes

---
+++ python_basics/operations_datatypes/slices_with_negative_indexes.py
@@ -0,0 +1,15 @@
+# accessing items and slices with negative indexes 
+monday_temperatures = [9.1, 8.8, 7.5, 6.6, 9.9]
+print(monday_temperatures[-1])
+
+# accessing first item from a list using negative indexes
+monday_temperatures = [9.1, 8.8, 7.5, 6.6, 9.9]
+print(monday_temperatures[-5])
+
+# accessing first two items using slices
+monday_temperatures = [9.1, 8.8, 7.5, 6.6, 9.9]
+print(monday_temperatures[:-3])
+
+# accessing first item using slices
+monday_temperatures = [9.1, 8.8, 7.5, 6.6, 9.9]
+print(monday_temperatures[:-4])
