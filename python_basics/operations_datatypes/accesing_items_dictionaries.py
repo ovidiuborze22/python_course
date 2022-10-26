7c3d7289ecd0bed54e9c48258a6d3440c95764dd
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-26 23:19:19 +0100

    accessing items in dictionaries

---
+++ python_basics/operations_datatypes/accesing_items_dictionaries.py
@@ -0,0 +1,6 @@
+# accessing items in dictionaries
+students_grades = {"Marry": 9.1, "Sim": 8.8, "John": 7.5}
+print(students_grades["Sim"])
+
+eng_port = {"rain": "chuva", "sun": "sol"}
+print(eng_port["sun"])
