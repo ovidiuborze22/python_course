7273c02acca009f2089e76206731d8b1bdf97a6a
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-26 22:38:31 +0100

    accessing items in list

--- python_basics/operations_datatypes/accesing_list_items.py
+++ python_basics/operations_datatypes/accesing_list_items.py
@@ -1,4 +1,21 @@
 
-# using getitem method
+# using getitem method to access an item in the list
 monday_temperatures = [9.1, 8.8, 7.5]
 print(monday_temperatures.__getitem__(1))
+
+
+# Complete the script so that it prints out the 3rd item of list serials
+serials = ["RH80810A", "AA899819A", "XYSA9099400", "OOP8988459", "EEO8904882", "KOC9889482"]
+print(serials[2])
+
+
+# Complete the script so that it prints out the 1st, 3rd, and the 6th items of the list serials
+serials = ["RH80810A", "AA899819A", "XYSA9099400", "OOP8988459", "EEO8904882", "KOC9889482"]
+print(serials[0], serials[2], serials[5])
+
+# Append the first item of weekend to workdays.
+workdays = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+weekend = ["Sat", "Sun"]
+
+workdays.append(weekend[0])
+print(workdays)
