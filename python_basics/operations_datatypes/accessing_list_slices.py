b7c74822c0900710895f3bf9663e9fe73a0b3188
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-26 22:50:54 +0100

    accessing list slices

---
+++ python_basics/operations_datatypes/accessing_list_slices.py
@@ -0,0 +1,15 @@
+# accessing list slices
+monday_temperatures = [9.1, 8.8, 6.6, 7.5, 9.9]
+print(len(monday_temperatures))
+
+# accessing list slices from 1-4
+print(monday_temperatures[1:4])
+
+# accessing list slices from 0-2
+print(monday_temperatures[0:2])
+
+# another way to access the first two items from list
+print(monday_temperatures[:2])
+
+# accessing the last two items from list
+print(monday_temperatures[3:])
