570b7a60112af96ceac98f1938b36d0a6ced14ef
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-28 15:42:18 +0100

    string formatting with multiple variables

---
+++ python_basics/user_input/string_formatting_with_multiple_variables.py
@@ -0,0 +1,8 @@
+# string formatting with multiple variables
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+when = "today?"
+
+message = "Hello %s %s" % (name, surname)
+message = f"Hello {name} {surname}. What's up {when}"
+print(message)
