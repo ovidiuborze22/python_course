7876116a75c26af92b53652f0e1306294145db8a
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-28 14:42:56 +0100

    understanding the use of white space

---
+++ python_basics/functions_and_conditionals/white_space.py
@@ -0,0 +1,19 @@
+# understating of white space
+
+if      3 > 1: # not a good practice
+    print('a')
+
+print('aa')
+print('aaa')
+
+if 3>1: # not a good practice
+    print('b')
+
+print('bb')
+print('bbb')
+
+if 3 > 1: # good practice 
+    print('c')
+
+print('cc')
+print('ccc')
