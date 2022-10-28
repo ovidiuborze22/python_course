2ef9c06d97ade7b6a06102307f92b5d6b1b1531c
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-28 18:24:39 +0100

    for loops

---
+++ python_basics/loops/for_loops.py
@@ -0,0 +1,16 @@
+# for loops how and why
+monday_temperatures = [9.1, 8.8, 7.6]
+
+# print(round(monday_temperatures[0]))
+# print(round(monday_temperatures[1]))
+# print(round(monday_temperatures[2]))
+
+for temperature in monday_temperatures:
+    print(round(temperature))
+
+print('Done\n')
+
+for letter in 'hello':
+    print(letter.title())
+
+print('Done')
