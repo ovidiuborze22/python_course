6d4a748a8edc2a8448fa6f6b527a8c380371a7a3
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-29 21:15:42 +0100

    function with multiple parameters

---
+++ python_basics/more_about_functions/function_with_multiple_parameters.py
@@ -0,0 +1,8 @@
+# Function with Multiple Parameters (E)
+# Implements a function that takes two strings as parameters, 
+# concatenates them, and returns the result.
+
+def foo(s1, s2):
+    return s1 + s2
+
+print(foo("Py","thon"))
