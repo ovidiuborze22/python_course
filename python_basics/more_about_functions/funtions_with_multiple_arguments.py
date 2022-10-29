fbf6139f6444617abb40986f4581352e393faa09
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-29 21:11:40 +0100

    understanding functions with multiple arguments

---
+++ python_basics/more_about_functions/funtions_with_multiple_arguments.py
@@ -0,0 +1,13 @@
+# functions with multiple arguments
+
+#>>> len("abc")
+#3
+#>>> isinstance("abc", str)
+#True
+#>>> help(len)
+#>>> help(isinstance)
+
+def area(a, b):
+    return a * b
+
+print(area(4, 5))
