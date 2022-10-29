70a8a3928704a9f77252f6db73c444957270cb39
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-29 19:58:14 +0100

    putting the pieces together by building a program

---
+++ python_basics/building_a_program/program.py
@@ -0,0 +1,22 @@
+# building the maker function
+def sentence_maker(phrase):
+    interrogatives = ("how", "what", "why")
+    capitalized = phrase.capitalize()
+    if phrase.startswith(interrogatives):
+        return "{}?".format(capitalized)
+    else:
+        return "{}.".format(capitalized)
+
+# print(sentence_maker("how are you"))
+
+# constructing the loop
+results = []
+while True:
+    user_input = input("Say something: ")
+    if user_input == "\end":
+        break
+    else:
+        results.append(sentence_maker(user_input))
+
+# making the output user-friendly
+print(" ".join(results))
