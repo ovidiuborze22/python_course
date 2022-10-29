5cc5f87025fed236d02718a45345958bfea24fc5
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-29 20:39:28 +0100

    list comprehension returning only positive numbers

---
+++ python_basics/list_comprehensions/only_positive_numbers.py
@@ -0,0 +1,8 @@
+# Only Positive Numbers (E)
+# Define a function that takes as parameter list of numbers and returns the list containing only the numbers that are greater than 0. 
+# For example, I called your function with foo([-5, 3, -1, 101]) it should return [3, 101].
+
+def foo(lst):
+    return [ i for i in lst if i > 0]
+
+print(foo([-5, 3, -1, 101]))
