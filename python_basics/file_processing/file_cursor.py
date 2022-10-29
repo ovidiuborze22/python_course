554b2837ccaf04562754e8922b32833e2e50267c
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-29 23:28:49 +0100

    file cursor

---
+++ python_basics/file_processing/file_cursor.py
@@ -0,0 +1,15 @@
+# file cursor
+myfile = open("python_course/python_basics/file_processing/fruits.txt")
+#print(myfile.read())
+#print(myfile.read())# cursor stops at the end
+
+# to fix this we will apply this method
+# store the content in a variable
+content = myfile.read()
+
+# now we can print as many times we want
+print(content)
+print(content)
+
+
+
