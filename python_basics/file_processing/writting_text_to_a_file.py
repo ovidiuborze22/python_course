dc34fa15aa3e7e4d6c5b72b361c980f3e3e9b2a2
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-30 20:39:45 +0000

    writing text to a file

---
+++ python_basics/file_processing/writting_text_to_a_file.py
@@ -0,0 +1,5 @@
+# writing text to a file
+
+with open("python_course/python_basics/file_processing/files/vegetables.txt", "w") as myfile:
+    myfile.write("Tomato\nCucumber\nOnion\n")
+    myfile.write("Garlic")
