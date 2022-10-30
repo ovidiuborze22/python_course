27adfe6881ea8db9aeaddfb377fe57c4e8468214
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-10-30 21:33:51 +0000

    creating a file with name file and writing the text snail there

---
+++ python_basics/file_processing/write_snail.py
@@ -0,0 +1,5 @@
+# Write Snail (E)
+# Use Python to create a file with name file.txt and write the text snail there.
+
+with open("python_course/python_basics/file_processing/files/file.txt", "w") as file:
+    file.write("snail")
