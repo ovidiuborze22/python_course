5f9be3899e7803ea51a6733ab8e9241db92e5443
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-11-05 22:46:56 +0000

    how to fix difficult errors

--- python_basics/fixing_programming_errors/difficult_errors.py
+++ python_basics/fixing_programming_errors/difficult_errors.py
@@ -8,4 +8,6 @@ print(c/0)
 # ZeroDivisionError: division by zero
 
 # Solution 1.Try to understand the error and fix it
-# Solution 2.copy this error and check on google for example stackowerflow website
+# Solution 2.Copy this error and check on google for example stackowerflow website
+# Solution 3.Ask a good programming question for example stackoverflow
+# Solution 4.Making the code to handle errors by itself
