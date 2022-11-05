de28d69e45ef8c23a3bc9cbb42722de7c07e00ce
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-11-05 18:03:08 +0000

    practicing file processing by adding markers from files

--- python_apps/App1_Web_Mapping_with_Python/map.py
+++ python_apps/App1_Web_Mapping_with_Python/map.py
@@ -2,13 +2,20 @@
 # interactive mapping of population and volcanoes
 
 import folium
+import pandas
+
+# loading data using pandas
+data = pandas.read_csv("python_course/python_apps/App1_Web_Mapping_with_Python/Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 # adding a marker to the map
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[38.2, -99.1],[39.2, -97.1]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Hi i am a marker", icon=folium.Icon(color="green")))
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi i am a marker", icon=folium.Icon(color="green")))
 
 map.add_child(fg)
 
