142bd6b0be231680dd1408f9139c02540273685c
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-11-05 19:24:48 +0000

    add and stylize markers

--- python_apps/App1_Web_Mapping_with_Python/map.py
+++ python_apps/App1_Web_Mapping_with_Python/map.py
@@ -24,7 +24,8 @@ map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(str(el)+" m",parse_html=True), icon=folium.Icon(color=color_producer(el))))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=folium.Popup(str(el)+" m",parse_html=True), 
+    fill_color=color_producer(el), color='gray',fill=True, fill_opacity=0.7))
 
 map.add_child(fg)
 
