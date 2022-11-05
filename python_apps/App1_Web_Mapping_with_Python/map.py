14a93818b3cdae5c6025e29ca5c4e6b742d93ef0
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-11-05 18:15:24 +0000

    practicing string manipulation by adding text on the map popup window

--- python_apps/App1_Web_Mapping_with_Python/map.py
+++ python_apps/App1_Web_Mapping_with_Python/map.py
@@ -8,14 +8,15 @@ import pandas
 data = pandas.read_csv("python_course/python_apps/App1_Web_Mapping_with_Python/Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 # adding a marker to the map
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi i am a marker", icon=folium.Icon(color="green")))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(str(el)+" m",parse_html=True), icon=folium.Icon(color="green")))
 
 map.add_child(fg)
 
