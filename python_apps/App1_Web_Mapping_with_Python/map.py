2b0d42e8a7141ae4b7df980fc7ccc564ad8d99d3
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-11-05 17:23:16 +0000

    practicing "for-loops" by adding multiple markers

--- python_apps/App1_Web_Mapping_with_Python/map.py
+++ python_apps/App1_Web_Mapping_with_Python/map.py
@@ -6,7 +6,10 @@ map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 # adding a marker to the map
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi i am a marker", icon=folium.Icon(color="green")))
+
+for coordinates in [[38.2, -99.1],[39.2, -97.1]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="Hi i am a marker", icon=folium.Icon(color="green")))
+
 map.add_child(fg)
 
-map.save("map.html")
+map.save("python_course/python_apps/App1_Web_Mapping_with_Python/map.html")
