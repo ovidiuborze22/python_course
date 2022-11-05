87f3e96d8542d929141653e30196fdc53c8883bd
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-11-05 20:18:31 +0000

    stylizing the population layer

--- python_apps/App1_Web_Mapping_with_Python/map.py
+++ python_apps/App1_Web_Mapping_with_Python/map.py
@@ -24,10 +24,12 @@ map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=folium.Popup(str(el)+" m",parse_html=True), 
-    fill_color=color_producer(el), color='gray',fill=True, fill_opacity=0.7))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(el)+" m", 
+    fill_color=color_producer(el), color='grey',fill=True, fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=(open('python_course/python_apps/App1_Web_Mapping_with_Python/world.json', 'r', encoding='utf-8-sig').read())))
+fg.add_child(folium.GeoJson(data=open('python_course/python_apps/App1_Web_Mapping_with_Python/world.json', mode='r', encoding='utf-8-sig').read(), 
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
 map.add_child(fg)
 
