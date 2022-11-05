94a58989bfdf591004783e5818a0f95af4afa373
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-11-05 21:27:04 +0000

    fixing syntax errors

---
+++ python_basics/fixing_programming_errors/syntax_errors.py
@@ -0,0 +1,20 @@
+# fixing programming errors
+print(1)
+int(9)
+int (999)
+# SyntaxError in line 4
+#  int 9
+#      ^
+# SyntaxError: invalid syntax
+print(2)
+print (3)
+# SyntaxError in line 10
+# print 3
+# ^^^^^^^
+# SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+
+a= [1,2,3]
+# SyntaxError in line 16
+# a= [1,2,3)
+#          ^
+# SyntaxError: closing parenthesis ')' does not match opening parenthesis '['
