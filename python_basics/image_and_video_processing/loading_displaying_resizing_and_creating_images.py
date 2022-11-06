da8039687d42328df1ed2c28cbf3bd1ed771640d
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-11-06 12:45:10 +0000

    loading, displaying, resizing and creating images

--- python_basics/image_and_video_processing/loading_displaying_resizing_and_creating_images.py
+++ python_basics/image_and_video_processing/loading_displaying_resizing_and_creating_images.py
@@ -2,19 +2,21 @@
 import cv2
 
 # loading an image
-img=cv2.imread("python_course/python_basics/image_and_video_processing/images/Moon sinking, sun rising.jpg",0)
+img=cv2.imread("python_course/python_basics/image_and_video_processing/Galaxy.jpg",0)
 
 print(type(img))
 print(img)
-#print(img.shape)
-#print(img.ndim)
+print(img.shape)
+print(img.ndim)
 
 # resizing an image
-# resized_image=cv2.resize(img,(int(img.shape[1]/2),int(img.shape[0]/2)))
-resized_image=cv2.resize(img,(100,100))
+resized_image=cv2.resize(img,(int(img.shape[1]/2),int(img.shape[0]/2)))
+# resized_image=cv2.resize(img,(100,100))
+
 # displaying an image
-cv2.imshow("Moon sinking, sun rising",resized_image)
+cv2.imshow("Galaxy",resized_image)
+
 # creating an image
-cv2.imwrite("python_course/python_basics/image_and_video_processing/images/moon sinking, sun rising_resized.jpg",resized_image)
+cv2.imwrite("python_course/python_basics/image_and_video_processing/Galaxy_resized.jpg",resized_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
