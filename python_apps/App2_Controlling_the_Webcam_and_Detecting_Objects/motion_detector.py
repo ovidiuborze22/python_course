66dad27d946e2bbf62bb80f9a213d4b9a89fb73e
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-11-06 18:29:53 +0000

    storing objects detection timestamps in a CSV file

--- python_apps/App2_Controlling_the_Webcam_and_Detecting_Objects/motion_detector.py
+++ python_apps/App2_Controlling_the_Webcam_and_Detecting_Objects/motion_detector.py
@@ -1,13 +1,17 @@
 # App2: controlling the webcam and detecting objects
-import cv2, time
+import cv2, time, pandas
+from datetime import datetime
 
 first_frame=None
+status_list=[None,None]
+times=[]
+df=pandas.DataFrame(columns=["Start", "End"])
 
 video=cv2.VideoCapture(0)
 
 while True:
     check, frame = video.read()
-
+    status=0
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     gray=cv2.GaussianBlur(gray,(21,21),0)
 
@@ -22,10 +26,19 @@ while True:
     (cnts,_)=cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 10000:
             continue
+        status=1
+
         (x, y, w, h)=cv2.boundingRect(contour)
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
+
+    status_list.append(status)
+
+    if status_list[-1]==1 and status_list[-2]==0:
+        times.append(datetime.now())
+    if status_list[-1]==0 and status_list[-2]==1:
+        times.append(datetime.now())
 
     cv2.imshow("Gray Frame",gray)
     cv2.imshow("Delta Frame",delta_frame)
@@ -34,11 +47,19 @@ while True:
 
 
     key=cv2.waitKey(1)
-    print(gray)
-    print(delta_frame)
 
     if key==ord('q'):
+        if status==1:
+            times.append(datetime.now())
         break
+
+print(status_list)
+print(times)
+
+for i in range (0,len(times),2):
+    df=df.append({"Start":times[i],"End":times[i+1]}, ignore_index=True)
+
+df.to_csv("python_course/python_apps/App2_Controlling_the_Webcam_and_Detecting_Objects/Times.csv")
 
 video.release()
 cv2.destroyAllWindows()
