da512f2ac4a641bec2ca48b1599a55b33d315917
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-11-11 21:31:08 +0000

    adding a pie chart to the web app

---
+++ python_apps/App3_Part2_Data_Analysis_and_Visualization_in-Browser_Interactive_Plots/7_rat_crs_pie_chart.py
@@ -0,0 +1,78 @@
+import justpy as jp
+import pandas
+from datetime import datetime
+from pytz import utc
+
+data = pandas.read_csv('python_course/python_apps/App3_Part2_Data_Analysis_and_Visualization_in-Browser_Interactive_Plots/data_files/reviews.csv', parse_dates=['Timestamp'])
+share = data.groupby(['Course Name'])['Rating'].count()
+
+chart_def = """
+{
+    chart: {
+        type: 'pie'
+    },
+    title: {
+        text: 'Pie Chart'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f}%',
+            }
+        }
+    },
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'Chrome',
+            y: 70.67,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'Edge',
+            y: 14.77
+        },  {
+            name: 'Firefox',
+            y: 4.86
+        }, {
+            name: 'Safari',
+            y: 2.63
+        }, {
+            name: 'Internet Explorer',
+            y: 1.53
+        },  {
+            name: 'Opera',
+            y: 1.40
+        }, {
+            name: 'Sogou Explorer',
+            y: 0.84
+        }, {
+            name: 'QQ',
+            y: 0.51
+        }, {
+            name: 'Other',
+            y: 2.6
+        }]
+    }]
+}
+"""
+def app():
+    wp = jp.QuasarPage()
+    h1 = jp.QDiv(a=wp, text="Analysis of Course Review", classes="text-h3 text-center q-pa-md")
+    p1 = jp.QDiv(a=wp, text="These graphs represent course review analysis")
+    
+    hc = jp.HighCharts(a=wp, options=chart_def)
+    hc_data = [{"name":v1, "y":v2} for v1,v2 in zip(share.index,share)]
+    #print(hc_data)
+    hc.options.series[0].data = hc_data
+   
+    return wp
+
+jp.justpy(app)
