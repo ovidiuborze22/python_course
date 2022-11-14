a51b55005bb0765c1c8707fa7071babfb85956c5
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-11-14 23:19:23 +0000

    displaying output to the user

--- python_apps/App6_Mobile_App_Development_Feel_Good_App/main.py
+++ python_apps/App6_Mobile_App_Development_Feel_Good_App/main.py
@@ -1,9 +1,12 @@
 # App6 Mobile App Development: Build a Feel Good App
+from random import random
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-import json
+import json, glob
 from datetime import datetime
+from pathlib import Path
+import random
 
 Builder.load_file('design.kv')
 
@@ -14,17 +17,18 @@ class LoginScreen(Screen):
     def login(self, uname, pword):
         with open("users.json") as file:
             users = json.load(file)
+
         if uname in users and users[uname]['password'] == pword:
             self.manager.current='login_screen_success'
         else:
-            self.ids.login_wrong.text='Wrong username or password' 
+            self.ids.login_wrong.text='Wrong username or password!' 
 
 class RootWidget(ScreenManager):
     pass
 
 class SignUpScreen(Screen):
     def add_user(self, uname, pword):
-        with open("users.json") as file:
+        with open("users.json", 'r') as file:
             users = json.load(file)
 
         users[uname]={'username': uname, 'password':pword,
@@ -32,18 +36,29 @@ class SignUpScreen(Screen):
         
         with open("users.json", 'w') as file:
             json.dump(users, file)
-        
         self.manager.current="sign_up_screen_success"
 
 class SignUpScreenSuccess(Screen):
     def go_to_login(self):
-        self.manager.current="login_screen"
         self.manager.transition.direction='right'
-
+        self.manager.current="login_screen"
+        
 class LoginScreenSuccess(Screen):
     def log_out(self):
         self.manager.transition.direction = 'right'
         self.manager.current="login_screen"
+
+    def get_quote(self, feel):
+        feel = feel.lower()
+        available_feelings= glob.glob("quotes/*txt")
+
+        available_feelings= [Path(filename).stem for filename in available_feelings ] 
+        if feel in available_feelings:
+            with open(f"quotes/{feel}.txt", encoding="utf8") as file:
+                quotes = file.readlines()
+            self.ids.quote.text= random.choice(quotes)
+        else:
+            self.ids.quote.text = "Try another feeling!"
 
 class MainApp(App):
     def build(self):
