29099f112a3e6df6176db905141dcda61a4e7ad4
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-11-25 15:39:15 +0000

    emailing database values back to user

--- python_apps/App8_Flask_and_PostGreSQL_Build_a_Data_Collector_Web_App/app.py
+++ python_apps/App8_Flask_and_PostGreSQL_Build_a_Data_Collector_Web_App/app.py
@@ -1,7 +1,7 @@
 # App8 Flask and PostGreSQL - Build a Data Collector Web App
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-
+from send_email import send_email
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:admin@localhost/height_collector'
@@ -26,7 +26,7 @@ def success():
     if request.method=='POST':
         email=request.form["email_name"]
         height=request.form["height_name"]
-        print(email, height)
+        send_email(email, height)
         if db.session.query(Data).filter(Data.email_==email).count()==0:
             data=Data(email,height)
             db.session.add(data)
