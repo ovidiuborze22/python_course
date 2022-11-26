db84fe77ccd94d88b7928acba2f628174edfdf3b
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-11-26 13:13:34 +0000

    listing blog posts on the homepage

--- python_apps/App9_Django_and_Bootstrap_Blog_and_Translator_App/mysite/settings.py
+++ python_apps/App9_Django_and_Bootstrap_Blog_and_Translator_App/mysite/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,12 +55,11 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'mysite.urls'
 
 
-TEMPLATES_DIRS = os.path.join(BASE_DIR, 'templates')
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIRS],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
