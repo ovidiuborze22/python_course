5b1caa98bef9f798f6c2a80e6d9d7c0c3bfbecff
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-11-26 15:02:51 +0000

    django template filters

--- python_apps/App9_Django_and_Bootstrap_Blog_and_Translator_App/mysite/settings.py
+++ python_apps/App9_Django_and_Bootstrap_Blog_and_Translator_App/mysite/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +54,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'mysite.urls'
 
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 TEMPLATES = [
     {
