25f1f6c5424a46f53bcebba1c149340d8328637f
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-11-26 17:37:53 +0000

    completing the translator app

--- python_apps/App9_Django_and_Bootstrap_Blog_and_Translator_App/translator/translate.py
+++ python_apps/App9_Django_and_Bootstrap_Blog_and_Translator_App/translator/translate.py
@@ -3,5 +3,5 @@ from googletrans import Translator
 
 def translate(text):
     translator = Translator()
-    translation = translator.translate(text=text, dest='hindi')
+    translation = translator.translate(text=text, dest='ro')
     return translation.text
