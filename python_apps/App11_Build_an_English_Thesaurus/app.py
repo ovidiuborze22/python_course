0732d0b5b63b22b41e2d4e1d249aaa6ac1a109f7
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-11-27 11:33:18 +0000

    getting confirmation from the user

--- python_apps/App11_Build_an_English_Thesaurus/app.py
+++ python_apps/App11_Build_an_English_Thesaurus/app.py
@@ -9,7 +9,13 @@ def translate(w):
     if w in data:
         return data[w]
     elif len(get_close_matches(w, data.keys())) >0:
-        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
+        yn = input("Did you mean %s instead? Enter Y is yes, or N if no." % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry." 
     else:
         return "The word doesn't exist. Please double check it."
 
