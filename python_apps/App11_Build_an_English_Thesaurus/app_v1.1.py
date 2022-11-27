4e900fabcc2d574826588a7df160a0424ed44848
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-11-27 12:09:08 +0000

    Making version 1.1 or the program

---
+++ python_apps/App11_Build_an_English_Thesaurus/app_v1.1.py
@@ -0,0 +1,30 @@
+# App11 Building an English Thesaurus
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+        # adding to the condition to check for lowercase y as well
+        if yn == "Y" or "y":
+            return data[get_close_matches(w, data.keys())[0]]
+         # adding to the condition to check for lowercase n as well
+        elif yn == "N" or "n":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
+    else:
+        return "The word doesn't exist. Please double check it."
+
+word = input("Enter word: ")
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
