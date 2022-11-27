3f7aa2219df369f874f01f56602ca14590460def
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-11-27 10:28:39 +0000

    returning the definition of a word

---
+++ python_apps/App11_Build_an_English_Thesaurus/app.py
@@ -0,0 +1,11 @@
+# App11 Building an English Thesaurus
+import json
+
+data = json.load(open("data.json"))
+
+def translate(w):
+    return data[w]
+
+word=input("Enter word: ")
+
+print(translate(word))
