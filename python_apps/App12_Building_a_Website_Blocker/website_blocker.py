b69ce7fc213493e156836cc4a7d8105559705b5a
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-11-27 12:42:59 +0000

    setting up the script

---
+++ python_apps/App12_Building_a_Website_Blocker/website_blocker.py
@@ -0,0 +1,10 @@
+# App12 Building a Website Blocker
+import time
+
+hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
+redirect = "127.0.0.1"
+website_list =["www.facebook.com", "facebook.com", "www.yahoo.com", "yahoo.com"]
+
+while True:
+    print(1)
+    time.sheep(5)
