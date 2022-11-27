d5aa12354c004afbef68d462c15f9b9f4eedb97a
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-11-27 13:40:04 +0000

    implementing the first part of the program

--- python_apps/App12_Building_a_Website_Blocker/website_blocker.py
+++ python_apps/App12_Building_a_Website_Blocker/website_blocker.py
@@ -2,6 +2,7 @@
 import time
 from datetime import datetime as dt
 
+hosts_temp="hosts"
 hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
 redirect = "127.0.0.1"
 website_list =["www.facebook.com", "facebook.com", "www.yahoo.com", "yahoo.com"]
@@ -9,6 +10,13 @@ website_list =["www.facebook.com", "facebook.com", "www.yahoo.com", "yahoo.com"]
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
         print("Working hours...")
+        with open(hosts_temp,'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+website+"\n")
     else:
         print("Fun hours...")
     time.sleep(5)
