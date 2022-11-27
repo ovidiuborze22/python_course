19f039420a54ba23cc302323c70a6749b43459e3
Ovidiu Borze <ovidiuborze22@gmail.com>
2022-11-27 11:20:13 +0000

    finding the most similar word from a group of words

--- python_apps/App11_Build_an_English_Thesaurus/app.py
+++ python_apps/App11_Build_an_English_Thesaurus/app.py
@@ -1,5 +1,6 @@
 # App11 Building an English Thesaurus
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -7,6 +8,8 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w, data.keys())) >0:
+        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
     else:
         return "The word doesn't exist. Please double check it."
 
